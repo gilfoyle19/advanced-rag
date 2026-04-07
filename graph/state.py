5ef89f9d059d9bcf41463fa71c386fa8045324f2
gilfoyle19 <kartheekc19@gmail.com>
2026-04-07 14:01:40 +0200

    initial setup and ingestion.py

---
+++ graph/state.py
@@ -0,0 +1,14 @@
+from typing import List, TypedDict
+
+class State(TypedDict):
+    """
+    Represents the state of our graph.
+    question:question
+    generation: LLM generation
+    web_search: whether to add search or not
+    documents: list of documents"""
+
+    question: str
+    generation: str
+    web_search: bool
+    documents: List[str]
